daa9d04904d5a6885866a1c16e5deb187149c245
conan-destemido <conan.destemido@gmail.com>
2020-04-24 15:51:26 -0300

    Paginação

--- index.php
+++ index.php
@@ -37,16 +37,34 @@ $app->get('/', function() {
 // Rota para exibir no site a categoria de um produto
 $app->get("/categories/:idcategory", function($idcategory){
 	
+	$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+	
 	$category = new Category();
 	
 	$category->get((int)$idcategory);
 	
-	$page = new Page();
+	$pagination = $category->getProductsPage($page);
 	
+	$pages = [];
+	
+	for($i = 1; $i <= $pagination["pages"]; $i++)
+	{
+		
+		array_push($pages, [
+			"link"=>"/categories/".$category->getidcategory()."?page=".$i,
+			"page"=>$i
+		]);	
+		
+	}
+	
+	$page = new Page();
+
 	$page->setTpl("category", [
 		"category"=>$category->getValues(),
-		"products"=>Product::checkList($category->getProducts())
+		"products"=>$pagination["data"],
+		"pages"=>$pages
 	]);
+	
 });
 
 // ROTA PARA OS ARQUIVOS DO ADMIN
